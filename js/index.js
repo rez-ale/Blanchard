974ac83c40745b7895525d28c7eeb70cf88e53eb
Aleksandr <29aleksan1998@mail.ru>
2022-08-30 21:17:36 +0300

    transfer_from_local_in_rep

--- js/index.js
+++ js/index.js
@@ -1,8 +1,58 @@
-const swiper = new Swiper('.swiper', {
+// heroSwiper
+const swiperHero = new Swiper('.hero__swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
   autoplay: {
-    delay: 4000,
+    delay: 2000,
   }
 });
+
+const swiperGallery = new Swiper('.gallery__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 3,
+  createElements: true,
+  loop: false,
+  spaceBetween: 50,
+  pagination: {
+    el: '.gallery__pagination',
+    type: 'fraction',
+  },
+  slidesPerGroup: 3,
+  navigation: {
+    nextEl: '.swiper-button-next-gallery',
+    prevEl: '.swiper-button-prev-gallery',
+  },
+
+});
+
+// swiperEvents
+const swiperEvents = new Swiper('.events__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  slidesPerView: 3,
+  createElements: true,
+  loop: false,
+  spaceBetween: 50,
+  slidesPerGroup: 1,
+  navigation: {
+    nextEl: '.swiper-button-next-events',
+    prevEl: '.swiper-button-prev-events',
+  },
+
+});
+
+// gallerySelect
+const element = document.querySelector('#filter');
+const choices = new Choices(element, {
+  searchEnabled: false,
+  itemSelectText: '',
+  duplicateItemsAllowed: false,
+  position: 'bottom',
+  sorter: () => {},
+});
+
+
+// galleryAccordion
+new Accordion('.accordion-container');
